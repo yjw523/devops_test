9f447809dc08d96b7b783a9323b86b941d94c34e
soongon <soongon@hucloud.co.kr>
2022-06-06 13:10:32 +0900

    Home Controller

--- src/main/java/kr/re/kitri/devopsdemo/DevopsDemoApplication.java
+++ src/main/java/kr/re/kitri/devopsdemo/DevopsDemoApplication.java
@@ -2,12 +2,18 @@ package kr.re.kitri.devopsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "kr.re.kitri")
-public class DevopsDemoApplication {
+@SpringBootApplication
+public class DevopsDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsDemoApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DevopsDemoApplication.class);
+    }
 }
