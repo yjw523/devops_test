bcb70375babca0fad96eb9ba3bf1a7ce1de32edd
soongon <soongon@hucloud.co.kr>
2022-06-06 12:23:23 +0900

    initial commit

---
+++ src/test/java/kr/re/kitri/devopsdemo/DevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.re.kitri.devopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
