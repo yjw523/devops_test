f2eda8339c196fc2af384a41a7bba44e1343018f
soongon <soongon@hucloud.co.kr>
2022-06-09 10:48:47 +0900

    커밋 쳐봤어요

--- src/main/java/kr/re/kitri/devopsdemo/controller/HomeController.java
+++ src/main/java/kr/re/kitri/devopsdemo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("")
     public String home() {
-        return "Hello World..";
+        return "Hello World.. ㅇㅏㄴ녕하세요.. 반갑습니다.";
     }
 
     @GetMapping("home")
