d58352172f7e2db6961fe564879616943b0a35cb
soongonkim <soongon@hucloud.co.kr>
2022-06-11 19:18:41 +0900

    initial commit

--- src/main/java/kr/re/kitri/devopsdemo/DevopsDemoApplication.java
+++ src/main/java/kr/re/kitri/devopsdemo/DevopsDemoApplication.java
@@ -6,14 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsDemoApplication extends SpringBootServletInitializer {
+public class DevopsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsDemoApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(DevopsDemoApplication.class);
-    }
 }
